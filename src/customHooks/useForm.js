b38cc3772622e71978251c35bf6bb8a64ce0cc4e
Eden <derhyeden@gmail.com>
2023-04-19 13:36:06 +0300

    fix package json

--- src/customHooks/useForm.js
+++ src/customHooks/useForm.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react'
+import { useState } from 'react'
 import { useEffectUpdate } from './useEffectUpdate'
 
 export function useForm(initialFields, cb = () => {}) {
